a81e1ee427de9c56b811a8da42c388769f1452b1
jerwintitus1999 <jerwintitus1999@gmail.com>
2024-10-07 23:53:32 +0530

    FrontEnd reactAPP added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from "./pages/login"
+import Register from "./pages/signup"
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {/* <Login/> */}
+        <Register/>
+      </div>
+    </>
+  )
+}
+
+export default App
