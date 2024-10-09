69b22fb093b875ac7f2a59ae79d6d47197032b0a
NAZEERAHMED7 <nazeerbossyt777@gmail.com>
2024-10-09 23:59:09 +0530

    updated navbar and hero section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,7 +15,7 @@ import { Routes, Route } from "react-router-dom"
 function App() {
 
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
