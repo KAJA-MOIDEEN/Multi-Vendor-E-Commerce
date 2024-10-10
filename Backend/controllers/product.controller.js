0996fb22b8988cec7165b19a949752069d91d439
kaja-moideen <kajamoideen3100@gmail.com>
2024-10-11 02:03:23 +0530

    Added Cloudinary integration for image uploads, updated product controller to handle multiple images, and modified multer middleware to specify upload destination

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -1,17 +1,44 @@
+import {v2 as cloudinary} from "cloudinary"
 
 // add product
 const addProduct = async (req,res)=>{
     try {
         const {name,description,price,category,subCategory,sizes,bestseller}  = req.body
-        const image1 =req.files.image1 || req.files.image1[0]
-        const image2 =req.files.image2 || req.files.image2[0]
-        const image3 =req.files.image3 || req.files.image3[0]
-        const image4 =req.files.image4 || req.files.image4[0]
-        console.log(name,description,price,image,category,subCategory,sizes,bestseller,date)
-        console.log(image1,image2,image3,image4);
-        
+
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        const images = [image1,image2,image3,image4].filter((item)=>item !== undefined)
+       
+        const imageUrl = await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:"image"});
+                return result.secure_url
+            }))
+            console.log(imageUrl);
+            
+            const productData = {
+                name,
+                description,
+                price:Number(price),
+                category,
+                subCategory,
+                sizes:JSON.parse(sizes),
+                bestseller: bestseller === "true" ? true : false,
+                image:imageUrl,
+                date:Date.now()
+            }
+            console.log(productData);
+            
+
+        res.json({
+            message:"product added successfully",
+        })
+
     } catch (error) {
-        console(error)
+        console.log(error)
         res.json({
             status: false,
             message: "Error adding product"
--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,11 +1,14 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');  // Set the destination folder for uploads
+      },
     filename:(req,file,cd)=>{
         cd(null,file.originalname)
     }
 })
 
-const upload = multer({storage})
+const upload = multer({storage:storage})
 
 export default upload
