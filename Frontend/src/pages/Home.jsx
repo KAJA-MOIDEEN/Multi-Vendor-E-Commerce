f9b902c02bca41637a4e522321fec139286e557c
jerwintitus1999 <jerwintitus1999@gmail.com>
2024-10-11 17:53:13 +0530

    BestSeller page updated and Footer page added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero'
 import LatestCollection from '@/components/LatestCollection'
 import NewsLetterBox from '@/components/NewsLetterBox'
 import OurPolicy from '@/components/OurPolicy'
+import BestSeller from '@/components/BestSeller'
 import React from 'react'
 
 const Home = () => {
