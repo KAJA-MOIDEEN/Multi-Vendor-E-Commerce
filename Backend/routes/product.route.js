cca54a47fcf6fac4e25e8e530877ecc2d21ca66b
vimalraj1812 <vimalmunusamy2002@gmail.com>
2024-10-11 16:18:36 +0530

    admin controllers login , admin authorized verified token add inside the product route control

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -6,12 +6,13 @@ import {
   singleProduct,
 } from "../controllers/product.controller.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/add",upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1},]),addProduct);
 productRouter.post("/remove",removeProduct);
-productRouter.get("/single",singleProduct);
+productRouter.post("/single",singleProduct);
 productRouter.get("/list", listProduct);
 
 
