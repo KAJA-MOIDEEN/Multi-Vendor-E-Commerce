82484b6f406980680590e7f1681a7c877e9c1a09
jerwintitus1999 <jerwintitus1999@gmail.com>
2024-10-29 17:07:54 +0530

    Dashboard for admin page is on process

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
@@ -30,6 +31,7 @@ const App = () => {
           <div className="w-[75%] mx-auto ml-[max(4vw,25px)] my-8 text-gray-600 text-base">
             <Routes>
               <Route path="/" element={<Add />} />
+              <Route path="/dashboard" element={<Dashboard/>}/>
               <Route path="/list" element={<List />} />
               <Route path="/orders" element={<Orders />} />
               <Route path= "/vendorsview" element={<VendorsView />} />
--- admin/src/assets/admin_assets/assets.js
+++ admin/src/assets/admin_assets/assets.js
@@ -7,6 +7,10 @@ import parcel_icon from './parcel_icon.svg'
 import profile_icon from './profile_icon.png'
 import view_icon from './view.png'
 import search_icon from './searchicon.png'
+import Graph_1 from './Graph_1.png'
+import Graph_2 from './Graph_2.png'
+import Graph_3 from './Graph_3.png'
+import Graph_4 from './Graph_4.png'
 
 export const assets = {
     Admin_logo,
@@ -17,6 +21,10 @@ export const assets = {
     parcel_icon,
     profile_icon,
     view_icon,
-    search_icon
+    search_icon,
+    Graph_1,
+    Graph_2,
+    Graph_3,
+    Graph_4
 }
 
